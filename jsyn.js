8fc37af92bcc769d8f6cde390f7f6f9e3bf7367f
Jim Palmer <jimpalmer@gmail.com>
2009-01-21 02:23:39 +0000

    more keywords - working on generic js/cs/php/etc keyword list fixed default single quote string to allow for escapes - we'll possibly need to do the same with backslash escaping on double-quote strings even though several languages support "" instead of \" or '' instead of \' for escaping want to unjqueryify this to make it completely generic - mimic the $.ready() and the pre.code selector. allow for specific languages, i.e. sql comments and python comments don't fit within the current generic "string" scope

--- jsyn.js
+++ jsyn.js
@@ -3,14 +3,15 @@
 		$('pre.code').each(
 			function () {
 var aa=new Date();
-				for (var k = 'function,var,class,using,namespace,public,private,protected,private,return,while,if,for,switch',
-						d = 'string,bool,date,int,float,hashtable,array',
+				for (var k = 'break,catch,class,continue,else,for,function,if,in,instanceof,namespace,private,private,protected,public,return,switch,throw,try,typeof,using,var,watch,while,new,' +
+							 'static,abstract,throw',
+						d = 'string,bool,boolean,date,datetime,int,int32,float,decimal,hashtable,array',
 						r = [
 							{c:'c',r:/(?:\/\*(.|[\n\r])*?\*\/)|(?:\/\/[^\n\r]+)|(?:<![-]{2,3}([\s\S](?!>))+[-]{2,3}>)/},	// comments
-							{c:'s',r:/(?:\'[^\']*\')|(?:"[^"]*")/},	// strings
+							{c:'s',r:/(?:'(?:[^']|\\')*[^\\]')|(?:"[^"]*")/},	// strings
 							{c:'n',r:/(?:\d+\.?\d*[%]?)/}, // numbers
-							{c:'k',r:(new RegExp('(?:' + k.split(',').join(')|(?:') + ')'))}, // keywords
-							{c:'d',r:(new RegExp('(?:' + d.split(',').join(')|(?:') + ')'))}, // datatypes
+							{c:'k',r:(new RegExp('(?:' + k.split(',').join('\\s)|(?:') + ')'))}, // keywords
+							{c:'d',r:(new RegExp('(?:' + d.split(',').join('\\s)|(?:') + ')'))}, // datatypes
 							{c:'w',r:/(?:[A-Za-z_-]\w*)/}, // word (variables)
 							{c:'f',r:/(?:[\[\]\(\)\{\}\/]+)/} // flow operators
 						],is = $(this).text(),os = '',re = '',rec = 0,rel = r.length; rec < rel; rec++ ) {
@@ -26,6 +27,7 @@ var aa=new Date();
 						}
 					}
 				}
+//alert(t.source);
 				$(this).replaceWith('<pre class="code">'+ os + is.substring(pi,is.length) +'</pre>\n'+ ((new Date()).getTime() - aa.getTime()) +'ms');
 			}
 		);
