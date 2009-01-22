bb0f12f312000de49da959dfa786cd6fdb29633b
jimpalmer <jimpalmer@60275904-e5e7-11dd-b00f-9fabb4b2ff13>
2009-01-22 08:08:08 +0000

    first attempt at diverse definitions basic web and sql definitions

--- jsyn.js
+++ jsyn.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
 	$('pre.code').each(function () {
-		for (var k = 'break,catch,class,continue,else,for,function,if,in,instanceof,namespace,private,protected,public,return,switch,throw,try,typeof,using,var,watch,while,new,static,abstract,throw',
-				d = 'string,bool,boolean,date,datetime,int,int32,float,decimal,hashtable,array',
-				r = [{c:'c',r:/(?:\/\*(.|[\n\r])*?\*\/)|(?:\/\/[^\n\r]+[\n\r])|(?:<![-]{2,3}([\s\S](?!>))+[-]{2,3}>)/},	// comments
+		var typ=this.className.split(' ').pop(), typs = { web:{
+				k:'break,case,catch,class,continue,delete,do,else,for,function,if,in,instanceof,new,'+
+					'private,protected,public,return,switch,throw,throw,try,typeof,var,watch,while',
+				d:'array,bool,boolean,date,datetime,decimal,false,float,hashtable,int,int32,nan,null,'+
+					'string,true',
+				c:/(?:\/\*(.|[\n\r])*?\*\/)|(?:\/\/[^\n\r]+[\n\r])|(?:<![-]{2,3}([\s\S](?!>))+[-]{2,3}>)/
+			}, sql:{
+				k:'alter,begin,by,commit,create,delete,drop,exec,from,group,having,insert,join,like,'+
+					'on,order,rollback,select,set,table,transaction,trigger,truncate,union,update,'+
+					'values,where',
+				d:'and,asc,between,desc,distinct,exists,inner,left,or,outer,right,top',
+				c:/(?:--[^\n\r]+[\n\r])/
+			}};
+		for (var r = [{c:'c',r:(typs[typ] || typs.web).c},	// comments
 					{c:'s',r:/(?:\/\S+\/)|(?:'(?:\\'|[^'])*')|(?:"(?:\\"|[^"])*")/},	// regexp,strings
 					{c:'n',r:/(?:\d+\.?\d*[%]?)/}, // numbers
-					{c:'k',r:(new RegExp('(?:' + k.split(',').join('\\s)|(?:') + ')'))}, // keywords
-					{c:'d',r:(new RegExp('(?:' + d.split(',').join('\\s)|(?:') + ')'))}, // datatypes
+					{c:'k',r:(new RegExp('(?:' + (typs[typ] || typs.web).k.split(',').join('\\s)|(?:') + ')','i'))}, // keywords
+					{c:'d',r:(new RegExp('(?:' + (typs[typ] || typs.web).d.split(',').join('\\s)|(?:') + ')','i'))}, // datatypes
 					{c:'w',r:/(?:[A-Za-z_-]\w*)/}, // word (variables)
 					{c:'f',r:/(?:[\[\]\(\)\{\}\/]+)/} // flow operators
 				],is = $(this).text(),os = '',re = '',rec = 0,rel = r.length; rec < rel; rec++ ) {
