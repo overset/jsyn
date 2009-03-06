4d41d50eb6c5fec7c4c1c4704d9f9a4c500a6473
jimpalmer <jimpalmer@60275904-e5e7-11dd-b00f-9fabb4b2ff13>
2009-03-06 01:37:12 +0000

    now configurable tab sizing(IE issues) and some more cleanup

--- jsyn.js
+++ jsyn.js
@@ -1,7 +1,8 @@
 /* jsyn - ultralight syntax highlighter; Jim Palmer - jimpalmer@gmail.com; released under MIT License */
 $(document).ready(function () {
 	$('pre.code').each(function () {
-		var ext = new Date(),typ=this.className.split(' ').pop(),typs = { web:{
+		// define syntax def types for tokenization
+		var ext = new Date(),types = { web:{
 				k:'break,case,catch,class,continue,delete,do,else,for,function,if,in,instanceof,new,'+
 					'private,protected,public,return,switch,throw,throw,try,typeof,var,watch,while',
 				d:'array,bool,boolean,date,datetime,decimal,false,float,hashtable,int,int32,nan,null,'+
@@ -13,23 +14,31 @@ $(document).ready(function () {
 					'values,where',
 				d:'and,asc,between,desc,distinct,exists,inner,left,or,outer,right,top',
 				c:/(?:\/\*(.|[\n\r])*?\*\/)|(?:--[^\n\r]+[\n\r])/
-			}};
-		for (var r = [{c:'c',r:(typs[typ] || typs.web).c},	// comments
+			}}, ct = types.web, dtab = 8, tabs = parseInt((this.className.match(/tab([0-9]+)/) || [])[1]) || dtab;
+		// find extra type definitions - otherwise use default web
+		for (var ty in types ) if (this.className.indexOf(types[ty]) >= 0) { ct = types[ty]; break; }
+		// build the tokenizing regexp
+		for (var r = [{c:'c',r:ct.c},	// comments
 					{c:'s',r:/(?:\/\S+\/)|(?:'(?:\\'|[^'])*')|(?:"(?:\\"|[^"])*")/}, // regexp,strings
 					{c:'n',r:/(?:\d+\.?\d*[%]?)/}, // numbers
-					{c:'k',r:(new RegExp('(?:'+ (typs[typ] || typs.web).k.split(',').join('\\s)|(?:') +')','i'))}, // keywords
-					{c:'d',r:(new RegExp('(?:'+ (typs[typ] || typs.web).d.split(',').join('\\s)|(?:') +')','i'))}, // datatypes
+					{c:'k',r:(new RegExp('(?:'+ ct.k.split(',').join('\\s)|(?:') +')'))}, // keywords
+					{c:'d',r:(new RegExp('(?:'+ ct.d.split(',').join('\\s)|(?:') +')'))}, // datatypes
 					{c:'w',r:/(?:[A-Za-z_-]\w*)/}, // word (variables)
 					{c:'f',r:/(?:[\[\]\(\)\{\}\/]+)/}, // flow operators
-					{c:'t',r:/(?:[\t])/} // flow operators
+					{c:'t',r:(new RegExp(( tabs == dtab ? '(?:\t)' : '(?:\t)' )))} // reformatted tabs
 				],is = $(this).text(),os = '',re = '',rec = 0,rel = r.length; rec < rel; rec++ ) {
-			re += (re ? '|' : '') + r[rec].r.source;
+			//re += ( ( re && r[rec].r.source ) ? '|' : '' ) + r[rec].r.source;
+			re += ( re ? '|' : '' ) + r[rec].r.source;
 		}
+		// wrap each token with appropriate type for colorization - using low-footprint inline element
 		for (var t = new RegExp(re,'gmi'),pi = 0,a = true,c = 0,of = 0,sl = is.length; c < sl && (a = t.exec(is)); c++) {
 			for (rec = 0; rec < rel; rec++) {
 				if (r[rec].r.test(a[0])) {
+					if (tabs != dtab && r[rec].c == 't')
+						var mp = t.lastIndex - a[0].length - 1,
+							to = tabs - ( ( mp - Math.max(is.lastIndexOf('\t', mp),is.lastIndexOf('\n', mp)) ) % tabs );
 					os += is.substring(pi,(t.lastIndex - a[0].length)).replace(/</g,'&lt;').replace(/>/g,'&gt;') +
-						'<span class="'+ r[rec].c +'">'+ a[0].replace(/</g,'&lt;').replace(/>/g,'&gt;') +'</span>';
+						'<b class="'+ r[rec].c + ( to && parseInt(to) != tabs ? ' t'+ to : '') +'">'+ a[0].replace(/</g,'&lt;').replace(/>/g,'&gt;') +'</b>';
 					pi = t.lastIndex;
 					break;
 				}
