92972438a5854d1cb225a87256de425dfa6b5949
Jim Palmer <jimpalmer@gmail.com>
2009-05-14 07:52:29 +0000

    attempt to rid of jquery dependency. still using document.ready. commented out DOM version instead of innerHTML - much slower

--- jsyn.js
+++ jsyn.js
@@ -1,24 +1,23 @@
 /* jsyn - ultralight syntax highlighter; Jim Palmer - jimpalmer@gmail.com; released under MIT License */
-var jsyn = function () {
-	$('pre.code').each(function () {
+var nodes = document.getElementsByTagName('pre'), jsyn = function () {
+	for (var node in nodes) {
+		var n = nodes[node], newPre = document.createElement('pre');
+		// only pre.code
+		if ((n.className || '' ).indexOf('code') < 0) continue;
 		// define syntax def types for tokenization
-		var ext = new Date(),types = { web:{
-				k:'break,case,catch,class,continue,delete,do,else,for,function,if,in,instanceof,new,'+
-					'private,protected,public,return,switch,throw,throw,try,typeof,var,watch,while',
-				d:'array,bool,boolean,date,datetime,decimal,false,float,hashtable,int,int32,nan,null,'+
-					'string,true',
+		var ext = new Date(), types = { web:{
+				k:'break,case,catch,class,continue,delete,do,else,for,function,if,in,instanceof,new,private,protected,public,return,switch,throw,throw,try,typeof,var,watch,while',
+				d:'array,bool,boolean,date,datetime,decimal,false,float,hashtable,int,int32,nan,null,string,true',
 				c:/(?:\/\*(.|[\n\r])*?\*\/)|(?:\/\/[^\n\r]+[\n\r])|(?:<![-]{2,3}([\s\S](?!>))+[-]{2,3}>)/
 			},sql:{
-				k:'alter,begin,by,commit,create,delete,drop,exec,from,group,having,insert,join,like,'+
-					'on,order,rollback,select,set,table,transaction,trigger,truncate,union,update,'+
-					'values,where',
+				k:'alter,begin,by,commit,create,delete,drop,exec,from,group,having,insert,join,like,on,order,rollback,select,set,table,transaction,trigger,truncate,union,update,values,where',
 				d:'and,asc,between,desc,distinct,exists,inner,left,or,outer,right,top',
 				c:/(?:\/\*(.|[\n\r])*?\*\/)|(?:--[^\n\r]+[\n\r])/
-			}}, ct = types.web, dtab = 8, tabs = parseInt((this.className.match(/tab([0-9]+)/) || [])[1]) || dtab;
+			}}, ct = types.web, dtab = 8, tabs = parseInt((n.className.match(/tab([0-9]+)/) || [])[1]) || dtab;
 		// find extra type definitions - otherwise use default web
-		for (var ty in types ) if (this.className.indexOf(types[ty]) >= 0) { ct = types[ty]; break; }
+		for (var ty in types ) if (n.className.indexOf(types[ty]) >= 0) { ct = types[ty]; break; }
 		// build the tokenizing regexp
-		for (var r = [{c:'c',r:ct.c},	// comments
+		for (var os = [], r = [{c:'c',r:ct.c},	// comments
 					{c:'s',r:/(?:\/\S+\/)|(?:'(?:\\'|[^'])*')|(?:"(?:\\"|[^"])*")/}, // regexp,strings
 					{c:'n',r:/(?:\d+\.?\d*[%]?)/}, // numbers
 					{c:'k',r:(new RegExp('(?:'+ ct.k.split(',').join('\\s)|(?:') +')'))}, // keywords
@@ -26,7 +25,7 @@ var jsyn = function () {
 					{c:'w',r:/(?:[A-Za-z_-]\w*)/}, // word (variables)
 					{c:'f',r:/(?:[\[\]\(\)\{\}:]+)/}, // flow operators
 					{c:'t',r:(new RegExp(( tabs == dtab ? '' : '(?:\t)' )))} // reformatted tabs
-				],is = $(this).text(),os = '',re = '',rec = 0,rel = r.length; rec < rel; rec++ ) {
+				],is = n.firstChild.nodeValue,re = '',rec = 0,rel = r.length; rec < rel; rec++ ) {
 			re += ( ( re && r[rec].r.source ) ? '|' : '' ) + r[rec].r.source;
 		}
 		// wrap each token with appropriate type for colorization - using low-footprint inline element
@@ -37,15 +36,33 @@ var jsyn = function () {
 						var mp = t.lastIndex - a[0].length - 1,
 							rn = Math.max(is.lastIndexOf('\r', mp), is.lastIndexOf('\n', mp))
 							to = tabs - ( ( mp - Math.max(is.lastIndexOf('\t', mp), rn) ) % tabs );
-					os += is.substring(pi,(t.lastIndex - a[0].length)).replace(/</g,'&lt;').replace(/>/g,'&gt;') +
+					// innerHTML method
+					os.push( is.substring(pi,(t.lastIndex - a[0].length)).replace(/</g,'&lt;').replace(/>/g,'&gt;') +
 						'<b class="'+ r[rec].c + ( r[rec].c == 't' && parseInt(to) != tabs ? to : '') +'">'+
-						a[0].replace(/</g,'&lt;').replace(/>/g,'&gt;') +'</b>';
+						a[0].replace(/</g,'&lt;').replace(/>/g,'&gt;') +'</b>' );
+/*
+*/
+/*
+					// DOM method
+					var nBlock = document.createElement('b');
+					nBlock.className = r[rec].c + ( r[rec].c == 't' && parseInt(to) != tabs ? to : '');
+					nBlock.appendChild(document.createTextNode(a[0]));
+					newPre.appendChild(document.createTextNode(is.substring(pi,(t.lastIndex - a[0].length))));
+					newPre.appendChild(nBlock);
+*/
 					pi = t.lastIndex;
 					break;
 				}
 			}
 		}
-		$(this).replaceWith('<pre class="'+ this.className +'">'+ os + is.substring(pi,is.length) +'</pre>');
-			//+ ((new Date()).getTime() - ext.getTime()) +'ms');
-	});
-}; $(jsyn);
+		// pre needed to render in IE6
+		n.innerHTML = (/msie/i.test(navigator.userAgent) ? '<pre>':'') + os.join('') + is.substring(pi,is.length) + ((new Date()).getTime() - ext.getTime()) +'ms'+(/msie/i.test(navigator.userAgent) ? '</pre>':'');
+/*
+		// DOM methods
+		n.removeChild(n.firstChild);
+		n.appendChild(newPre);
+		n.appendChild(document.createTextNode(is.substring(pi,is.length) + ((new Date()).getTime() - ext.getTime()) +'ms'));
+*/
+	}
+};
+$(jsyn);
